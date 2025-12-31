955b33f4637c5a0ed50ee23ae2d0dff876221211
stalechilibeans <73906828+stalechilibeans@users.noreply.github.com>
2025-12-30 23:31:36 -0500

    more thi still no collision lots of cleanup still needed

--- levels/thi/header.h
+++ levels/thi/header.h
@@ -14,7 +14,7 @@ extern const GeoLayout thi_geo_00079C[];
 
 // leveldata
 extern const Gfx big_world_1_dl_mesh[];
-extern const Gfx thi_seg7_dl_07006968[];
+extern const Gfx big_world_2_dl_mesh[];
 extern const Gfx thi_seg7_dl_07007008[];
 extern const Gfx thi_seg7_dl_070072E8[];
 extern const Gfx thi_seg7_dl_070073C0[];
