8678c9f4d13026621929a64a648a8f37a0c7c4f4
The1UPGuy <75804989+The1UPGuy@users.noreply.github.com>
2026-07-19 20:24:09 -0300

    update ddd areamap

--- levels/ddd/areas/1/areamap.inc.c
+++ levels/ddd/areas/1/areamap.inc.c
@@ -1,7 +1,7 @@
 struct AreaMapData water_land_area_1_areamap = {
     /* Texture Top */ ddd_seg7_texture_areamap_area_1_top,
     /* Texture Bottom */ ddd_seg7_texture_areamap_area_1_bottom,
-    /* Arrow X */      0.0f,
-    /* Arrow Y */     -2.5f,
-    /* Size */        1.25f,
+    /* Arrow X */     -3.0f,
+    /* Arrow Y */      0.0f,
+    /* Size */         1.0f,
 };
--- levels/ddd/areas/2/areamap.inc.c
+++ levels/ddd/areas/2/areamap.inc.c
@@ -1,7 +1,7 @@
 struct AreaMapData water_land_area_2_areamap = {
     /* Texture Top */ ddd_seg7_texture_areamap_area_2_top,
     /* Texture Bottom */ ddd_seg7_texture_areamap_area_2_bottom,
-    /* Arrow X */     -1.25f,
-    /* Arrow Y */      1.25f,
-    /* Size */         1.25f,
+    /* Arrow X */      4.5f,
+    /* Arrow Y */      0.0f,
+    /* Size */         1.0f,
 };
